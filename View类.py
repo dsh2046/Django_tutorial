3b5ef257745510d1e1d2bbea382a87ad22261c11
Samuel <dsh20462046@gmail.com>
2017-07-06 13:48:31 -0400

    Update View类.py

--- View类.py
+++ View类.py
@@ -1,16 +1,19 @@
 from django.views.generic.base import View   # urls.py:  url(r'^login/', LoginView.as_view(), name='login')
+from .forms import LoginForm
 
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html', {})
 
     def post(self, request):
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html', {'username': username})
+        login_form = LoginForm(request.POST)  # 用Django的Ｆｏｒｍ来验证合法性
+        if login_form.is_valid():
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html', {'username': username})
         else:
             return render(request, 'login.html', {})
         
